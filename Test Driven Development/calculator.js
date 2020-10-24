398614a437bc7adacb07bcb6fc280bd311960e4d
Melvin <melvin.ocs@gmail.com>
2020-10-24 15:16:51 +0800

    adding new files from learning

---
+++ Test Driven Development/calculator.js
@@ -0,0 +1,30 @@
+/**
+ * Function adds two arguments that contain / return numbers of any type 
+ * @param {} x 
+ * @param {*} y 
+ */
+
+/**
+ * Write an addition function for following tests
+ * 1. Two numbers
+ * 2. Two number arrays
+ * 3. Two functions that return numbers
+ * @param {} x 
+ * @param {*} y 
+ */
+
+function add(x, y) {
+    return x + y;
+}
+
+function addTwoArrays([array1], [array2]) {
+    return array1 + array2; //add the number in two arrays and return the sum
+}
+
+function addNumbersFromTwoFunctions(number1, number2){
+    return number1 + number2;
+}
+
+function subtract(a, b){
+    return a - b;
+}
